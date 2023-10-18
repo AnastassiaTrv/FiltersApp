023e1473bfee041d7a449bb7ac3d889d307c273f
Anastassia Tretjakova <anastassia.tretjakova@elisa.ee>
2023-10-18 11:11:23 +0300

    project setup

---
+++ filtersapp-backend/src/main/java/org/anatrv/filtersapp/FiltersappApplication.java
@@ -0,0 +1,13 @@
+package org.anatrv.filtersapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiltersappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiltersappApplication.class, args);
+	}
+
+}
---
+++ filtersapp-backend/src/test/java/org/anatrv/filtersapp/FiltersappApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anatrv.filtersapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltersappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
